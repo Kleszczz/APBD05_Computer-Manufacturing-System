057590fd639a12c02ce9efd3474160b0497fafb3
Kleszczz <mijoj.koworow@gmail.com>
2026-05-16 14:59:37 +0200

    add AppDb context and first test controller

---
+++ APBD05_Computer-Manufacturing-System/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using APBD05_Computer_Manufacturing_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APBD05_Computer_Manufacturing_System.Data;
+
+public class AppDbContext : DbContext
+{
+    protected AppDbContext()
+    {
+        
+    }
+    
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Component> Components { get; set; }
+    public DbSet<ComponentType> ComponentTypes { get; set; }
+    public DbSet<ComponentManufacturer> ComponentManufacturers { get; set; }
+    public DbSet<PC> PCs { get; set; }
+    public DbSet<PCComponent> PCComponents { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
